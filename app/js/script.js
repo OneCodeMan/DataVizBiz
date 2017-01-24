3ee2fb51f63d0ec9daa1ad205e6ceb149ffbb4a1
Dave Gumba <dave_gumba@Yahoo.com>
2017-01-23 21:27:31 -0500

    Got a basic graph working. Now to make it into a circle graph.

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,33 @@
-// https://plot.ly/javascript/ajax-call/
+function makeplot() {
+  Plotly.d3.csv('data/anime.csv', function(data) {
+    processData(data);
+  });
+};
 
+function processData(allRows) {
+  console.log(allRows);
+  var x = [];
+  var y = [];
+  var standard_deviation = [];
 
-Plotly.d3.csv('data/anime.csv', function(err, rows) {
-
-  function unpack(rows, key) {
-  	return rows.map(function(row) { return row[key] });
+  for (var i = 0; i < allRows.length; i++) {
+    row = allRows[i];
+    x.push( row['episodes'] );
+    y.push( row['rating'] );
   }
+  console.log('X', x, 'Y', y, 'SD', standard_deviation);
+  makePlotly(x, y, standard_deviation);
 
-  var data;
+}
+function makePlotly(x, y, standard_deviation) {
+  var plotDiv = document.getElementById('plot');
+  var traces = [{
+    x: x,
+    y: y
+  }];
 
-});
+  Plotly.newPlot('display', traces,
+    {title: 'Anime'});
+  };
+
+makeplot();
